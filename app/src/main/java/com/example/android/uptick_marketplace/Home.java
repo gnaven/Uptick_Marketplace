a15b0dc0ceec8290bcd278beab6b1e77fd5bb4c7
Gazi Mahir Ahmed Naven <naven.gman@gmail.com>
2019-08-14 23:06:03 -0400

    integrated home page grid view functions

--- app/src/main/java/com/example/android/uptick_marketplace/Home.java
+++ app/src/main/java/com/example/android/uptick_marketplace/Home.java
@@ -9,8 +9,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Home extends Fragment {
+
+    private GridView gvGallery;
+    private GridBaseAdapter gridBaseAdapter;
+    private ArrayList<ImageModel> imageModelArrayList;
+
+    private int[] myImageList = new int[]{
+            R.drawable.fridge,R.drawable.fridge,
+            R.drawable.printer};
+    private String[] myImageNameList = new String[]{"Fridge", "Bag",
+            "Printer"};
 
     @Nullable
     @Override
@@ -18,9 +33,39 @@ public class Home extends Fragment {
         return inflater.inflate( R.layout.activity_home,null);
     }
 
+    private ArrayList<ImageModel> populateList(){
+
+        ArrayList<ImageModel> list = new ArrayList<>();
+
+        for(int i = 0; i < 3; i++){
+            ImageModel imageModel = new ImageModel();
+            imageModel.setName(myImageNameList[i]);
+            imageModel.setImage_drawable(myImageList[i]);
+            list.add(imageModel);
+        }
+
+        return list;
+    }
+
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        //Calling GridBasAdapter to populate images in the homepage
+        gvGallery = view.findViewById(R.id.gv);
+
+        imageModelArrayList = populateList();
+
+        gridBaseAdapter = new GridBaseAdapter(view.getContext(),imageModelArrayList);
+        gvGallery.setAdapter(gridBaseAdapter);
+
+        gvGallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText((getActivity()), myImageNameList[position]+" Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         view.findViewById(R.id.home_text);
         // Send to sell page
